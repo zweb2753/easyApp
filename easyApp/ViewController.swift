a70225d5091740b8cba663f1cac9a7f34237bd73
zweb2753 <31513803+zweb2753@users.noreply.github.com>
2017-10-10 08:29:53 -0600

    Created easyApp and wrote some code and edited gitignore.

--- easyApp/ViewController.swift
+++ easyApp/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  easyApp
+//  easyApp0
 //
 //  Created by Weber, Zane on 10/10/17.
 //  Copyright © 2017 CTEC. All rights reserved.
@@ -8,11 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var firstButton: UIButton!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    private func creatRandomColor() -> UIColor
+    {
+       let newColor :UIColor
+       let redAmount = CGFloat (Double (arc4random_uniform(256))/255.00)
+       let greenAmount = CGFloat (Double (arc4random_uniform(256))/255.00)
+       let blueAmount = CGFloat (Double (arc4random_uniform(256))/255.00)
+       newColor = UIColor(red: redAmount, green: greenAmount, blue: blueAmount, alpha: CGFloat(1.0))
+    
+    
+       return newColor
     }
 
     override func didReceiveMemoryWarning() {
