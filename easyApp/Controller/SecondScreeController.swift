f579c515a1f7a404207953dfb372c9d81c440666
zweb2753 <31513803+zweb2753@users.noreply.github.com>
2017-10-12 08:52:54 -0600

    added a controller and sce to the storyboard.

---
+++ easyApp/Controller/SecondScreeController.swift
@@ -0,0 +1,14 @@
+//
+//  SecondScreeController.swift
+//  easyApp
+//
+//  Created by Weber, Zane on 10/12/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class SecondScreenController : UIViewController
+{
+    
+}
