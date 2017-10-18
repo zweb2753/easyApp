21afdf4c0af9b9012a1177a8e9d30d50d850ab90
zweb2753 <31513803+zweb2753@users.noreply.github.com>
2017-10-18 09:14:11 -0600

    Added a image and mp files and added a new media controller screen.

---
+++ easyApp/Controller/MediaPageController.swift
@@ -0,0 +1,72 @@
+//
+//  MediaPageController.swift
+//  easyApp
+//
+//  Created by Weber, Zane on 10/18/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class MediaPageController: UIViewController
+{
+
+    private var soundPlayer : AVAudioPlayer?
+    private var imageCounter : Int = 0
+    private lazy var color : ColorTools = ColorTools()
+    
+    @IBOutlet weak var imageFrame: UIImageView!
+    @IBOutlet weak var ImageButton: UIButton!
+    @IBOutlet weak var soundButton: UIButton!
+    @IBOutlet weak var oundSlider: UISlider!
+    
+    @IBOutlet weak var Soundbutton: UISlider!
+    @IBAction func changePicure() -> Void
+    {
+        switchImage()
+    }
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    private func switchImage() -> Void
+    {
+        if (imageCounter > 2)
+        {
+            imageCounter = 0
+        }
+        if (imageCounter == 0)
+        {
+            imageFrame.image = UIImage(named: "Tree")
+        }
+        else if (imageCounter == 1)
+        {
+            imageFrame.image = UIImage(named: "Bed")
+        }
+        else
+        {
+            imageFrame.image = UIImage(named: "Food")
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
