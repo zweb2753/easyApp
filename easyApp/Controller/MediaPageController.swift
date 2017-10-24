e77a4897bd20a46f4dd9e0d19b1f553cda7659df
zweb2753 <31513803+zweb2753@users.noreply.github.com>
2017-10-24 08:56:32 -0600

    added audio player and the und file as well as other additiones

--- easyApp/Controller/MediaPageController.swift
+++ easyApp/Controller/MediaPageController.swift
@@ -19,7 +19,7 @@ class MediaPageController: UIViewController
     @IBOutlet weak var imageFrame: UIImageView!
     @IBOutlet weak var ImageButton: UIButton!
     @IBOutlet weak var soundButton: UIButton!
-    @IBOutlet weak var oundSlider: UISlider!
+    @IBOutlet weak var soundSlider: UISlider!
     
     @IBOutlet weak var Soundbutton: UISlider!
     @IBAction func changePicure() -> Void
@@ -35,10 +35,11 @@ class MediaPageController: UIViewController
     
     private func switchImage() -> Void
     {
-        if (imageCounter > 2)
+        if (imageCounter > 4)
         {
             imageCounter = 0
         }
+        
         if (imageCounter == 0)
         {
             imageFrame.image = UIImage(named: "Proj Pic Tree")
@@ -47,10 +48,12 @@ class MediaPageController: UIViewController
         {
             imageFrame.image = UIImage(named: "Proj Pic Bed")
         }
-        else
+        else if (imageCounter == 2)
         {
             imageFrame.image = UIImage(named: "Proj Pic Food")
         }
+        view.backgroundColor = color.createRandomColor();
+        imageCounter += 1
     }
 
     override func didReceiveMemoryWarning() {
@@ -58,6 +61,69 @@ class MediaPageController: UIViewController
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func soundPlay()
+    {
+        playMusicFile()
+    }
+    
+    private func playMusicFile() -> Void
+    {
+        if let isPlaying = soundPlayer?.isPlaying
+        {
+            if (isPlaying)
+            {
+                soundPlayer?.pause()
+            }
+            else
+            {
+                soundPlayer?.pause()
+            }
+        }
+        
+        private func loadAudioFile() -> Void
+        {
+            if let soundUL = NSDataAsset(name: "Nero_Promises")
+            {
+               do
+               {
+                   try! AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+                   try! AVAudioSession.sharedInstance().setActive(true)
+                
+                try soundPlayer = AVAudioPlayer(data: soundUrl.data, fileTypeHint:
+                    AVFileType.mp3.rawValue
+                soundSlider.maximumValue = Float ((soundPlayer?.duration)!)
+                Timer.scheduldTimer(timeInterval: 0.2, target: self, selector:
+                    (#selector(self.updateSlider)), userInfo: nil, repeats: true)
+                }
+                catch
+                {
+                    print("Auio File Load Error")
+                }
+           }
+      }
+        
+        @objc private func updateSlider() -> Void
+        {
+            soundSlider.value = Float ((soundPlayer?.currentTime)!)
+        }
+        
+        @IBAction func slidingSound(_ sender: UISlide)
+        {
+            let seekTime = Double (soundSlider.value)
+            soundPlayer.currentTime = seekTime
+        }
+    }
+        //this code is less safe
+        //if (soundPlayer?.isPlaying)!
+        //  {
+        //      soundPlayer?.pause()
+        // }
+        // else
+        // {
+        //     soundPlayer?.play()
+        //  }
+        
+    }
 
     /*
     // MARK: - Navigation
